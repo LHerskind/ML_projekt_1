e7d25767a25bd500fa166a8051fa97c0448cc697
Nanochrome <lasse.herskind@gmail.com>
2017-09-22 14:17:41 +0200

    Plots and code-structure added

--- LoadCarsData.py
+++ LoadCarsData.py
@@ -1,27 +1,66 @@
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from scipy.linalg import svd
-from matplotlib.pyplot import figure, plot, title, xlabel, ylabel, show
-
-file = "Cars-file-nice.txt";
-datamatrix = np.loadtxt(file)
-datamatrix[:,0] = datamatrix[:,0] * -1
-
-datamatrix_std = StandardScaler().fit_transform(datamatrix)
-
-cov_matrix = np.cov(datamatrix_std.T)
-coff_matrix = np.corrcoef(datamatrix_std.T)
-
-U, S, V = svd(datamatrix_std, full_matrices=False)
-rho = (S*S)/(S*S).sum()
-
-figure()
-plot(range(1,len(rho)+1),rho, 'o-')
-
-show()
+from matplotlib import pyplot as plt
+#from matplotlib.pyplot import figure, plot, title, xlabel, ylabel, show
 
 
-## PCA
+def load_from_file(file):
+    datamatrix = np.loadtxt(file)
+    datamatrix[:, 0] = datamatrix[:, 0] * -1
+    return datamatrix
+
+
+def convert_using_1_to_k(inputmatrix):
+    return np.hstack((inputmatrix[:, :7], np.reshape(get_one_to_k_matrix(),(len(datamatrix),3)) ))
+
+
+def get_one_to_k_matrix():
+    return [vectorized(e) for e in datamatrix[:,7]]
+
+
+def vectorized(j):
+    e = np.zeros((3,1))
+    e[int(j)-1] = 1.0
+    return e
+
+
+def std_cov_coff_matrices(datamatrix):
+    datamatrix_std = StandardScaler().fit_transform(datamatrix)
+    cov_matrix = np.cov(datamatrix_std.T)
+    coff_matrix = np.corrcoef(datamatrix_std.T)
+    return datamatrix_std, cov_matrix, coff_matrix
+
+
+def svd_graph(datamatrix_std):
+    U, S, V = svd(datamatrix_std, full_matrices=False)
+    rho = (S*S)/(S*S).sum()
+    rho_cummulative = np.cumsum(rho)
+
+    with plt.style.context('seaborn-whitegrid'):
+        plt.figure(figsize=(6,4))
+        plt.bar(range(1,len(rho)+1), rho, alpha=0.6, align='center', label='Individual explained variance')
+        plt.step(range(1,len(rho)+1), rho_cummulative, where='mid', label='Cumulative explained variance')
+        plt.ylabel("Explained variance ratio")
+        plt.xlabel('Principal components')
+        plt.legend(loc='best')
+        plt.tight_layout()
+
+        plt.show()
+
+
+
+
+
+if __name__ == '__main__':
+    file = "Cars-file-nice.txt";
+    datamatrix = load_from_file(file)
+    datamatrix = convert_using_1_to_k(datamatrix)
+    datamatrix_std, cov, coff = std_cov_coff_matrices(datamatrix)
+    svd_graph(datamatrix_std)
+
+
+'''  ## PCA
 eig_vals, eig_vecs = np.linalg.eig(cov_matrix)
 eig_pairs = [(np.abs(eig_vals[i]), eig_vecs[:, i]) for i in range(len(eig_vals))]
 eig_pairs.sort(key=lambda x: x[0], reverse=True)
@@ -34,8 +73,6 @@ print(eig_pairs[0][1].shape)
 for i in range(1, PCAs):
     matrix_w = np.hstack((matrix_w, eig_pairs[i][1].reshape(8, 1)))
 
-
-
-
 #for line in file_object.readlines():
 #   print (line)
+'''
