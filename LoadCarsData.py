e4546c41e8a6731421d7b731b5e1bc3a2cac48e3
Nanochrome <lasse.herskind@gmail.com>
2017-10-26 12:45:51 +0200

    Assignment 2 start

--- LoadCarsData.py
+++ LoadCarsData.py
@@ -1,8 +1,14 @@
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from scipy.linalg import svd
+from sklearn import model_selection
+import sklearn.linear_model as lm
 from mpl_toolkits.mplot3d import Axes3D
+from sklearn import cross_validation
 from matplotlib import pyplot as plt
+from toolbox_02450 import feature_selector_lr, bmplot
+from matplotlib.pyplot import figure, plot, subplot, title, xlabel, ylabel, show, clim
+
 #from matplotlib.pyplot import figure, plot, title, xlabel, ylabel, show
 
 attributeNames = ['MPG','Cylinders','Displacment','Horsepower','Weight (lbs)','Acceleration (MPH)','Model year','Origin']
@@ -127,6 +133,105 @@ def svd_graph(datamatrix_std, made1_to_k):
     plt.title('Origins show across PC1 & PC2')
     plt.show()
 
+def split_train_test(input_matrix ,index):
+    y = input_matrix[:,index];
+    X = np.delete(input_matrix, index, axis=1)
+    print (X.shape)
+    return X,y
+
+def linear_reg(input_matrix,index, outer_cross_number, inner_cross_number):
+    X, y = split_train_test(input_matrix, index)
+    N,M = X.shape
+    K = outer_cross_number
+    #CV = model_selection.KFold(K,True)
+    temp = attributeNames[index]
+    attributeNamesShorter = attributeNames
+    attributeNamesShorter.remove(temp)
+    CV = cross_validation.KFold(N, K, shuffle=True)
+
+    Features = np.zeros((M, K))
+    Error_train = np.empty((K, 1))
+    Error_test = np.empty((K, 1))
+    Error_train_fs = np.empty((K, 1))
+    Error_test_fs = np.empty((K, 1))
+    Error_train_nofeatures = np.empty((K, 1))
+    Error_test_nofeatures = np.empty((K, 1))
+
+    k = 0
+    for train_index, test_index in CV:
+        X_train = X[train_index, :]
+        y_train = y[train_index]
+        X_test = X[test_index, :]
+        y_test = y[test_index]
+        internal_cross_validation = inner_cross_number
+
+        Error_train_nofeatures[k] = np.square(y_train-y_train.mean()).sum()/y_train.shape[0]
+        Error_test_nofeatures[k] = np.square(y_test-y_test.mean()).sum()/y_test.shape[0]
+
+        m = lm.LinearRegression(fit_intercept=True).fit(X_train, y_train)
+        Error_train[k] = np.square(y_train-m.predict(X_train)).sum()/y_train.shape[0]
+        Error_test[k] = np.square(y_test-m.predict(X_test)).sum()/y_test.shape[0]
+        textout = '';
+        selected_features, features_record, loss_record = feature_selector_lr(X_train, y_train, internal_cross_validation,
+                                                                              display=textout)
+
+        Features[selected_features, k] = 1
+        # .. alternatively you could use module sklearn.feature_selection
+        if len(selected_features) is 0:
+            print('No features were selected, i.e. the data (X) in the fold cannot describe the outcomes (y).')
+        else:
+            m = lm.LinearRegression(fit_intercept=True).fit(X_train[:, selected_features], y_train)
+            Error_train_fs[k] = np.square(y_train - m.predict(X_train[:, selected_features])).sum() / y_train.shape[0]
+            Error_test_fs[k] = np.square(y_test - m.predict(X_test[:, selected_features])).sum() / y_test.shape[0]
+
+            figure(k)
+            subplot(1, 2, 1)
+            plot(range(1, len(loss_record)), loss_record[1:])
+            xlabel('Iteration')
+            ylabel('Squared error (crossvalidation)')
+
+            subplot(1, 3, 3)
+            bmplot(attributeNames, range(1, features_record.shape[1]), -features_record[:, 1:])
+            clim(-1.5, 0)
+            xlabel('Iteration')
+
+        print('Cross validation fold {0}/{1}'.format(k + 1, K))
+        print('Train indices: {0}'.format(train_index))
+        print('Test indices: {0}'.format(test_index))
+        print('Features no: {0}\n'.format(selected_features.size))
+
+        k += 1
+
+    figure(k)
+    subplot(1, 3, 2)
+    bmplot(attributeNamesShorter, range(1, Features.shape[1] + 1), -Features)
+    clim(-1.5, 0)
+    xlabel('Crossvalidation fold')
+    ylabel('Attribute')
+
+    # Inspect selected feature coefficients effect on the entire dataset and
+    # plot the fitted model residual error as function of each attribute to
+    # inspect for systematic structure in the residual
+
+    f = 2  # cross-validation fold to inspect
+    ff = Features[:, f - 1].nonzero()[0]
+    if len(ff) is 0:
+        print('\nNo features were selected, i.e. the data (X) in the fold cannot describe the outcomes (y).')
+    else:
+        m = lm.LinearRegression(fit_intercept=True).fit(X[:, ff], y)
+
+        y_est = m.predict(X[:, ff])
+        residual = y - y_est
+
+        figure(k + 1)
+        title('Residual error vs. Attributes for features selected in cross-validation fold {0}'.format(f))
+        for i in range(0, len(ff)):
+            subplot(2, np.ceil(len(ff) / 2.0), i + 1)
+            plot(X[:, ff[i]], residual, '.')
+            xlabel(attributeNamesShorter[ff[i]])
+            ylabel('residual error')
+
+        show()
 
 if __name__ == '__main__':
     made1_to_k = True
@@ -137,29 +242,10 @@ if __name__ == '__main__':
     if(made1_to_k):
         datamatrix_k = convert_using_1_to_k(datamatrix)
 
-
-
-    datamatrix_std, cov, coff = std_cov_coff_matrices(datamatrix_k)
-
-    create_plots(datamatrix, datamatrix_std)
+    #datamatrix_std, cov, coff = std_cov_coff_matrices(datamatrix_k)
     #create_plots(datamatrix_std, datamatrix_std)
+    #svd_graph(datamatrix_std, made1_to_k)
+    linear_reg(datamatrix, 3, 10, 10)
 
-    svd_graph(datamatrix_std, made1_to_k)
 
 
-'''  ## PCA
-eig_vals, eig_vecs = np.linalg.eig(cov_matrix)
-eig_pairs = [(np.abs(eig_vals[i]), eig_vecs[:, i]) for i in range(len(eig_vals))]
-eig_pairs.sort(key=lambda x: x[0], reverse=True)
-
-PCAs = 4
-
-print(eig_pairs[0][1].shape)
-
-#matrix_w = eig_pairs[0][1].reshape(8, 1)
-for i in range(1, PCAs):
-    matrix_w = np.hstack((matrix_w, eig_pairs[i][1].reshape(8, 1)))
-
-#for line in file_object.readlines():
-#   print (line)
-'''
